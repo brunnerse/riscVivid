9dfc82be8b33df090d107278ca41aeeefd16f67d
Severin Brunner <severin.brunner@outlook.de>
2019-05-13 20:38:21 +0200

    implemented better interrupt implementation; WB gets executed first, ClockCycleFrame can now display bubble instructions as empty rows

--- src/riscVivid/RiscVividSimulator.java
+++ src/riscVivid/RiscVividSimulator.java
@@ -74,7 +74,6 @@ public class RiscVividSimulator
     private Properties config;
     private Statistics stat;
     private boolean caught_break = false;
-    private boolean interrupt_occured_last_cycle = false;
     private int clock_cycle;
     private int sim_cycles;
     private boolean finished;
@@ -354,24 +353,29 @@ public class RiscVividSimulator
 
         boolean stall = false;
 
-        // Interrupt handling: flush the pipeline, start from the instruction subsequent to the scall
-        if (interrupt_occured_last_cycle) {
-    		uint32 nextPc = memory_writeback_latch.element().getPc();
-    		logger.debug("INTERRUPT IN LAST CYCLE: pipeline was flushed, restarting the subsequent instruction at " +
-    				nextPc.getValueAsHexString());
+        // Executing the WB-Stage first:
+        // simulates WB writing in the first half of the cycle and ID reading in the second half of the cycle
+        // also, makes handling interrupts easier
+        
+        // WRITE BACK STAGE
+        wod = pipeline.getWriteBackStage().doCycle();
+        caught_break = wod.getCaughtBreak();
+        // WRITE BACK STAGE
 
-    		// flush the pipeline
+        // Interrupt handling: flush the pipeline, start from the instruction subsequent to the scall
+        if (wod.getInterruptOccured()) {
+    		logger.debug("INTERRUPT: pipeline is being flushed, restarting the subsequent instruction at " +
+    				execute_memory_latch.element().getPc() + " in the next cycle");
+    		// flush the pipeline except memory_writeback_latch, as it has already been executed
     		fetch_decode_latch.element().flush();
     		decode_execute_latch.element().flush();
     		execute_memory_latch.element().flush();
-    		memory_writeback_latch.element().flush();
     		// flush execute_branchprediciton_latch, branchprediction_execute_latch and branchprediction_fetch_latch
-    		// by replacing them with dummies
+    		// by replacing their content with dummies
 	        uint32 zero = new uint32(0x0);
 	        Instruction bubble =  pipeline.getDecodeStage().decodeInstr(PipelineConstants.PIPELINE_BUBBLE_INSTR);
 	        // add 1 bubble into the branch prediction module
 	        ExecuteBranchPredictionData ebd = new ExecuteBranchPredictionData(bubble, zero, zero, false);
-
     		execute_branchprediction_latch.remove();
     		execute_branchprediction_latch.add(ebd);
     		branchprediction_execute_latch.remove();
@@ -379,9 +383,10 @@ public class RiscVividSimulator
     		branchprediction_fetch_latch.remove();
     		branchprediction_fetch_latch.add(new BranchPredictionModuleFetchData(false, zero, zero));
 
-    		// feed the Fetch Stage a jump to the instruction after the scall
-			ExecuteFetchData efd = new ExecuteFetchData(writeback_latch.element().getInst(),
-					writeback_latch.element().getPc(), nextPc, true, true);
+    		// feed the Fetch Stage a jump to the scall so it continues with the subsequent instruction in the next cycle
+			ExecuteFetchData efd = new ExecuteFetchData(memory_writeback_latch.element().getInst(),
+					memory_writeback_latch.element().getPc(),
+					memory_writeback_latch.element().getPc(), true, true);
     		execute_fetch_latch.remove();
 	        execute_fetch_latch.add(efd);
         }
@@ -392,6 +397,13 @@ public class RiscVividSimulator
         fod = pipeline.getFetchStage().doCycle();
         // FETCH STAGE
 
+        if (wod.getInterruptOccured()) {
+        	// flush output of fetch stage
+        	fod.getFdd().flush();
+        	for (int i = 0; i < fod.getFlush().length; ++i) {
+        		fod.getFlush()[i] = false;
+        	}
+        }
         // LATCH
         if (fod.getFlush()[PipelineConstants.DECODE_STAGE])
         {
@@ -424,20 +436,11 @@ public class RiscVividSimulator
         bpmod = pipeline.getBranchPredictionModule().doCycle();
         // BRANCH PREDICTOR MODULE: lookup for jump target and update prediction tables
 
-        // in case there's a syscall in the current cycle, suppress writing data
-        if (pipeline.getWriteBackStage().isSyscall(memory_writeback_latch.element().getInst()))
-        	execute_memory_latch.element().getInst().setStore(false);
         // MEMORY STAGE
         mod = pipeline.getMemoryStage().doCycle();
         // MEMORY STAGE
 
         // LATCH
-
-        // WRITE BACK STAGE
-        wod = pipeline.getWriteBackStage().doCycle();
-        caught_break = wod.getCaughtBreak();
-        interrupt_occured_last_cycle = wod.getInterruptOccured();
-        // WRITE BACK STAGE
 
         if (!stall)
         {
